7db82c9c3b92ae316bb42bd9b2e0feefc9da66cf
robel_ephraim <ephraimrobel15@gmail.com>
2021-03-05 14:17:23 +0300

    Signup and Login Component Added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="w-screen flex item-center justify-center">
     <router-link to="/" class="px-4">Home</router-link> |
-    <router-link to="/about" class="px-4">About</router-link>
+    <router-link to="/about" class="px-4">About</router-link> |
+    <router-link to="/signup" class="px-4">Signup</router-link> |
+    <router-link to="/login" class="px-4">Login</router-link>
+
   </div>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,19 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: () =>
+      import("../views/Signup.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import("../views/Login.vue")
+  },
 ];
 
 const router = createRouter({
