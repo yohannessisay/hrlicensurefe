444a920400edff1504d136c023bb76249351aa00
AbenezerYohannesAbebe <abenezer.yohannes.mail@gmail.com>
2021-03-05 05:16:37 +0300

    tailwind css configuration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="w-screen flex items-center justify-center">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
