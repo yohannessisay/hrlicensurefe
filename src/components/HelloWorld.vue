33d64e7093fd99817192946a4ebddfbae3005843
AbenezerYohannesAbebe <abenezer.yohannes.mail@gmail.com>
2021-03-14 05:46:32 +0300

    make all svg illustration vue components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="w-screen flex flex-col justify-center items-center">
     <div class="flex flex-col mt-large w-3/12">
-      <button>Default</button>
-      <br />
-      <button variant="outline">Outline</button><br />
-      <button variant="block">Block</button><br />
-      <button variant="outline-block">Outline Block</button>
+      <RenderIllustration />
     </div>
   </div>
 </template>
+<script>
+import RenderIllustration from "@/sharedComponents/RenderIllustration";
+export default {
+  components: { RenderIllustration },
+  setup() {}
+};
+</script>
---
+++ src/sharedComponents/RenderIllustration.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <Address v-if="illustration === Address" />
+    <Certificate v-if="illustration === Certificate" />
+    <Contact v-if="illustration === Contact" />
+    <GoodStanding v-if="illustration === GoodStanding" />
+    <Id v-if="illustration === Id" />
+    <Institution v-if="illustration === Institution" />
+    <Logo v-if="illustration === Logo" />
+    <MOH v-if="illustration === MOH" />
+    <PersonalInfo v-if="illustration === PersonalInfo" />
+    <Renewal v-if="illustration === Renewal" />
+    <SimpleCertificate v-if="illustration === SimpleCertificate" />
+    <Success v-if="illustration === Success" />
+    <User v-if="illustration === User" />
+    <Verification v-if="illustration === Verification" />
+  </div>
+</template>
+<script>
+import Address from "./illustrations/address";
+import Certificate from "./illustrations/certificate";
+import Contact from "./illustrations/contact";
+import GoodStanding from "./illustrations/goodStanding";
+import Id from "./illustrations/id";
+import Institution from "./illustrations/institution";
+import Logo from "./illustrations/logo";
+import MOH from "./illustrations/MOH";
+import PersonalInfo from "./illustrations/personalInfo";
+import Renewal from "./illustrations/renewal";
+import SimpleCertificate from "./illustrations/simpleCertificate";
+import Success from "./illustrations/success";
+import User from "./illustrations/user";
+import Verification from "./illustrations/verification";
+
+export default {
+  components: {
+    Address,
+    Certificate,
+    Contact,
+    GoodStanding,
+    Id,
+    Institution,
+    Logo,
+    MOH,
+    PersonalInfo,
+    Renewal,
+    SimpleCertificate,
+    Success,
+    User,
+    Verification
+  },
+  props: {
+    illustration: {
+      type: String
+    }
+  }
+};
+</script>
+<style lang="postcss" scoped>
+svg {
+  height: 20px;
+}
+</style>
