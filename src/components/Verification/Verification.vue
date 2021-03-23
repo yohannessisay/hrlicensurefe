4805de0e60a0538827c8d606f379c8e70fb8f5f6
Eyouel Ameneshwa <Eyouelame123@gmail.com>
2021-03-23 14:49:23 +0300

    Added Navigation to Components

--- src/components/Verification/Verification.vue
+++ src/components/Verification/Verification.vue
@@ -1,101 +1,108 @@
 <template>
-  <div
-    class="w-screen h-full bg-lightBlueB-200 flex items-center justify-center"
-  >
-    <div class="w-screen max-w-4xl mt-medium h-screen">
-      <div
-        class="flex flex-col mt-large w-full bg-white blue-box-shadow-light rounded mb-large"
-      >
-        <div class="mt-large">
-          <TitleWithIllustration
-            illustration="Verification"
-            message="Good Standing"
-          />
-        </div>
-        <form class="mx-auto max-w-3xl w-full mt-10" @submit.prevent="nextStep">
-          <div class="flex">
-            <div class="flex flex-col mb-medium w-1/2 mr-12">
-              <label class="text-primary-700">Name of inistitution</label>
-              <select class="max-w-3xl" v-model="selected">
-                <option disabled value="">Please select one</option>
-                <option>Value one</option>
-                <option>I'm the second one</option>
-                <option>Select me</option>
-              </select>
-            </div>
-            <div class="flex flex-col mb-medium w-1/2 m1-12">
-              <label class="text-primary-700">Department</label>
-              <select class="max-w-3xl" v-model="selected">
-                <option disabled value="">Please select one</option>
-                <option>Value one</option>
-                <option>I'm the second one</option>
-                <option>Select me</option>
-              </select>
-            </div>
+  <div>
+    <Navigation />
+    <div
+      class="w-screen h-full bg-lightBlueB-200 flex items-center justify-center"
+    >
+      <div class="w-screen max-w-4xl mt-medium h-screen">
+        <div
+          class="flex flex-col mt-large w-full bg-white blue-box-shadow-light rounded mb-large"
+        >
+          <div class="mt-large">
+            <TitleWithIllustration
+              illustration="Verification"
+              message="Good Standing"
+            />
           </div>
-          <div class="flex">
-            <div class="flex flex-col mb-medium w-1/2 mr-12">
-              <label class="text-primary-700">Inistitution Email</label>
-              <select class="max-w-3xl" v-model="selected">
-                <option disabled value="">Please select one</option>
-                <option>Value one</option>
-                <option>I'm the second one</option>
-                <option>Select me</option>
-              </select>
-            </div>
-            <div class="flex flex-col mb-medium w-1/2 m1-12">
-              <label class="text-primary-700">Application Institution</label>
-              <select class="max-w-3xl" v-model="selected">
-                <option disabled value="">Please select one</option>
-                <option>Value one</option>
-                <option>I'm the second one</option>
-                <option>Select me</option>
-              </select>
-            </div>
-          </div>
-          <div class="flex">
-            <div class="flex mb-small mr-12">
-              <div class="flex flex-col mb-medium mr-4">
-                <label class="text-primary-700"> Institution Type</label>
-                <div class="flex items-center mb-small">
-                  <input type="radio" id="moh" value="moh" />
-                  <label class="ml-tiny text-primary-700" for="moh">Public</label>
-                </div>
-                <div class="flex items-center mb-small">
-                  <input type="radio" id="hris" value="hris" />
-                  <label class="ml-tiny text-primary-700" for="hris">Private</label>
-                </div>
-                <div class="flex items-center mb-small">
-                  <input type="radio" id="fbc" value="fbc" />
-                  <label class="ml-tiny text-primary-700" for="fbc">
-                    Faith based or Charity
-                  </label>
-                </div>
+          <form
+            class="mx-auto max-w-3xl w-full mt-10"
+            @submit.prevent="nextStep"
+          >
+            <div class="flex">
+              <div class="flex flex-col mb-medium w-1/2 mr-12">
+                <label class="text-primary-700">Name of inistitution</label>
+                <select class="max-w-3xl" v-model="selected">
+                  <option disabled value="">Please select one</option>
+                  <option>Value one</option>
+                  <option>I'm the second one</option>
+                  <option>Select me</option>
+                </select>
+              </div>
+              <div class="flex flex-col mb-medium w-1/2 m1-12">
+                <label class="text-primary-700">Department</label>
+                <select class="max-w-3xl" v-model="selected">
+                  <option disabled value="">Please select one</option>
+                  <option>Value one</option>
+                  <option>I'm the second one</option>
+                  <option>Select me</option>
+                </select>
               </div>
             </div>
-            <div class="flex w-1/2 mb-small m1-12"></div>
-          </div>
-          <div class="flex justify-center mb-medium">
-            <div>
-              <button>Submit Request</button>
+            <div class="flex">
+              <div class="flex flex-col mb-medium w-1/2 mr-12">
+                <label class="text-primary-700">Inistitution Email</label>
+                <select class="max-w-3xl" v-model="selected">
+                  <option disabled value="">Please select one</option>
+                  <option>Value one</option>
+                  <option>I'm the second one</option>
+                  <option>Select me</option>
+                </select>
+              </div>
+              <div class="flex flex-col mb-medium w-1/2 m1-12">
+                <label class="text-primary-700">Application Institution</label>
+                <select class="max-w-3xl" v-model="selected">
+                  <option disabled value="">Please select one</option>
+                  <option>Value one</option>
+                  <option>I'm the second one</option>
+                  <option>Select me</option>
+                </select>
+              </div>
             </div>
-            <div>
-              <button variant="outline">
-                Save as Draft
-              </button>
+            <div class="flex">
+              <div class="flex mb-small mr-12">
+                <div class="flex flex-col mb-medium mr-4">
+                  <label class="text-primary-700"> Institution Type</label>
+                  <div class="flex items-center mb-small">
+                    <input type="radio" id="moh" value="moh" />
+                    <label class="ml-tiny text-primary-700" for="moh">Public</label>
+                  </div>
+                  <div class="flex items-center mb-small">
+                    <input type="radio" id="hris" value="hris" />
+                    <label class="ml-tiny text-primary-700" for="hris">Private</label>
+                  </div>
+                  <div class="flex items-center mb-small">
+                    <input type="radio" id="fbc" value="fbc" />
+                    <label class="ml-tiny text-primary-700" for="fbc">
+                      Faith based or Charity
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="flex w-1/2 mb-small m1-12"></div>
             </div>
-          </div>
-        </form>
+            <div class="flex justify-center mb-medium">
+              <div>
+                <button>Submit Request</button>
+              </div>
+              <div>
+                <button variant="outline">
+                  Save as Draft
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Navigation from "@/views/Navigation";
 import TitleWithIllustration from "@/sharedComponents/TitleWithIllustration";
 
 export default {
-  components: { TitleWithIllustration },
+  components: { TitleWithIllustration, Navigation },
   setup() {}
 };
 </script>
--- src/views/License.vue
+++ src/views/License.vue
@@ -1,63 +1,64 @@
 <template>
-  <Navigation/>
-  <div class="mt-8 ml-20">
-    <h4>Select License Type</h4>
+  <div>
+    <Navigation />
+    <div class="bg-lightBlueB-200 h-screen">
+      <div class="flex pl-24 pt-8 ">
+        <h2 class="text-lightBlueB-400">Select License Type</h2>
+      </div>
+      <div class="flex justify-center mt-8 rounded ">
+        <div class="container mr-12 box-shadow-pop rounded-lg bg-lightGrey-100 ">
+          <div class="flex justify-center items-center w-64 h-40">
+            <div class="p-1">
+              <div class="flex content-center justify-center">
+                <router-link to="/newlicense">
+                  <RenderIllustration illustration="Certificate" />
+                </router-link>
+              </div>
+              <h4 class="text-lightBlueB-500">New License</h4>
+            </div>
+          </div>
+        </div>
+
+        <div class="container mr-12 box-shadow-pop rounded-lg bg-lightGrey-100 ">
+          <div class="flex justify-center items-center w-64 h-40">
+            <div class="p-1">
+              <div class="my-auto flex justify-center">
+                <router-link to="/renewal">
+                  <RenderIllustration illustration="Renewal" />
+                </router-link>
+              </div>
+              <h4 class="text-lightBlueB-500">License Renewal</h4>
+            </div>
+          </div>
+        </div>
+
+        <div class="container mr-12 box-shadow-pop rounded-lg bg-lightGrey-100 ">
+          <div class="flex justify-center items-center w-64 h-40">
+            <div class="p-1">
+              <div class="my-auto flex justify-center">
+                <router-link to="/verification">
+                  <RenderIllustration illustration="Verification" />
+                </router-link>
+              </div>
+              <h4 class="text-lightBlueB-500">Verification</h4>
+            </div>
+          </div>
+        </div>
+
+        <div class="container mr-12 box-shadow-pop rounded-lg bg-lightGrey-100 ">
+          <div class="flex justify-center items-center w-64 h-40">
+            <div class="p-1">
+              <div class="my-auto flex justify-center">
+                <router-link to="/goodstanding">
+                  <RenderIllustration illustration="GoodStanding" />
+                </router-link>
+              </div>
+              <h4 class="text-lightBlueB-500">GoodStanding</h4>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
-  <div class="flex justify-center mt-8">
-    <div
-      style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"
-      variant="borderRadius"
-      class="container mr-12"
-    >
-      <div class="flex justify-center w-64 h-40">
-        <div class="p-1">
-          <div class="mt-4">
-            <router-link to="/newlicense">
-              <RenderIllustration illustration="GoodStanding" />
-            </router-link>
-          </div>
-        </div>
-      </div>
-
-      <div class="container mr-12 box-shadow-pop rounded-lg bg-lightGrey-100 ">
-        <div class="flex justify-center items-center w-64 h-40">
-          <div class="p-1">
-            <div class="my-auto flex justify-center">
-              <router-link to="/renewal">
-                <RenderIllustration illustration="Renewal" />
-              </router-link>
-            </div>
-            <h4 class="text-lightBlueB-500">License Renewal</h4>
-          </div>
-        </div>
-      </div>
-
-      <div class="container mr-12 box-shadow-pop rounded-lg bg-lightGrey-100 ">
-        <div class="flex justify-center items-center w-64 h-40">
-          <div class="p-1">
-            <div class="my-auto flex justify-center">
-              <router-link to="/verification">
-                <RenderIllustration illustration="Verification" />
-              </router-link>
-            </div>
-            <h4 class="text-lightBlueB-500">Verification</h4>
-          </div>
-        </div>
-      </div>
-
-      <div class="container mr-12 box-shadow-pop rounded-lg bg-lightGrey-100 ">
-        <div class="flex justify-center items-center w-64 h-40">
-          <div class="p-1">
-            <div class="my-auto flex justify-center">
-              <router-link to="/goodstanding">
-                <RenderIllustration illustration="GoodStanding" />
-              </router-link>
-            </div>
-            <h4 class="text-lightBlueB-500">GoodStanding</h4>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
