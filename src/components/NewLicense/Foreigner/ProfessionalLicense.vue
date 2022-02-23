0707c47fd5268ae2bf3e8bbd3f4f0f8e85228b92
robel_ephraim <ephraimrobel15@gmail.com>
2022-02-23 16:13:25 +0300

    add cancel button for newlicense

--- src/components/NewLicense/Foreigner/ProfessionalLicense.vue
+++ src/components/NewLicense/Foreigner/ProfessionalLicense.vue
@@ -33,6 +33,7 @@
         <span class="flex justify-center">{{ documentMessage }}</span>
         <div class="ml-24">
           <button @click="addDocs()">Add Document</button>
+          <button @click="removeDocs()">Remove Document</button>
         </div>
         <form @submit.prevent="submit" class="mx-auto max-w-3xl w-full mt-8">
           <div class="flex justify-center">
@@ -317,6 +318,11 @@ export default {
     const addDocs = () => {
       if (docCount.value < 2) {
         docCount.value++;
+      }
+    };
+    const removeDocs = () => {
+      if (docCount.value > 0) {
+        docCount.value--;
       }
     };
 
@@ -923,6 +929,7 @@ export default {
       maxSizeMB,
       docCount,
       addDocs,
+      removeDocs,
     };
   },
 };
