e92d9f3459b12f7c6b32f95a5c4ba88b993fe25a
Yohannes Sisay <yohannessisay90@gmail.com>
2022-10-19 15:19:34 +0300

    returned folder weird bug

---
+++ src/components/Reviewer/HomeComponents/Renewal/Returned/returned.vue
@@ -0,0 +1,367 @@
+<template>
+  <!-- Sidebar -->
+  <reviewer-side-nav />
+  <!-- Sidebar -->
+
+  <section class="home-section">
+    <!-- Header -->
+    <reviewer-nav-bar>
+      <ol class="list-reset flex">
+          <li>
+            <router-link to="/admin/review"
+              ><span class="text-primary-600 text-base">Home</span></router-link
+            >
+          </li>
+          <li><span class="text-gray-500 mx-2">/</span></li>
+          <li>
+            <a href="#" class="hover:text-primary-600 text-grey-300"
+              >Renewal</a
+            >
+          </li>
+          <li><span class="text-gray-500 mx-2">/</span></li>
+          <li>
+            <a href="#" class="pointer-events-none text-lg text-grey-300"
+              >Returned</a
+            >
+          </li>
+        </ol>
+    </reviewer-nav-bar>
+    <!-- Header -->
+
+    <!-- Main Content -->
+    <div class="home-content">
+      <new-license-main-content>
+        <template v-slot:toyou>
+          <div class="container mx-auto px-4 sm:px-8">
+            <div class="py-8">
+              <div>
+                <h2 class="text-2xl font-semibold leading-tight">
+                  Applications Returned By You
+                </h2>
+              </div>
+              <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div
+                  class="
+                    inline-block
+                    min-w-full
+                    shadow-md
+                    rounded-lg
+                    overflow-hidden
+                    bg-primary-800
+                  "
+                >
+                  <vue-table-lite
+                    :is-static-mode="true"
+                    :is-loading="toYouTable.isLoading"
+                    :columns="toYouTable.columns"
+                    :rows="toYouTable.rows"
+                    :total="toYouTable.totalRecordCount"
+                    :sortable="toYouTable.sortable"
+                    @is-finished="tableLoadingFinish"
+                    @row-clicked="rowClicked"
+                  ></vue-table-lite>
+                  <edit-modal v-if="showModal" :modalDataId="modalDataId">
+                  </edit-modal>
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+        <template v-slot:to_others>
+          <div class="container mx-auto px-4 sm:px-8">
+            <div class="py-8">
+              <div>
+                <h2 class="text-2xl font-semibold leading-tight">
+                  Applications Returned By Others
+                </h2>
+              </div>
+              <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div
+                  class="
+                    inline-block
+                    min-w-full
+                    shadow-md
+                    rounded-lg
+                    overflow-hidden
+                    bg-primary-800
+                  "
+                >
+                  <vue-table-lite
+                    :is-static-mode="true"
+                    :is-loading="toOthersTable.isLoading"
+                    :columns="toOthersTable.columns"
+                    :rows="toOthersTable.rows"
+                    :total="toOthersTable.totalRecordCount"
+                    :sortable="toOthersTable.sortable"
+                    @is-finished="tableLoadingFinishOthers"
+                    @row-clicked="rowClickedOthers"
+                  ></vue-table-lite>
+                  <edit-modal-others
+                    :modalDataIdOthers="modalDataIdOthers"
+                  ></edit-modal-others>
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </new-license-main-content>
+    </div>
+    <!-- Main Content -->
+  </section>
+</template>
+
+<script>
+import ReviewerSideNav from "../SharedComponents/sideNav.vue";
+import ReviewerNavBar from "../../../SharedComponents/navBar.vue";
+import NewLicenseMainContent from "../../../SharedComponents/returned.vue";
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex";
+import VueTableLite from "../../../../../plugins/TableLite.vue";
+import editModal from "./returnedModal.vue";
+import editModalOthers from "./returnedModalOthers.vue";
+import applicationStatus from "../../../Configurations/getApplicationStatus.js";
+
+export default {
+  name: "home",
+  components: {
+    ReviewerSideNav,
+    ReviewerNavBar,
+    NewLicenseMainContent,
+    VueTableLite,
+    editModal,
+    editModalOthers,
+  },
+  setup() {
+    const store = useStore();
+    const showModal = ref(true);
+    const adminId = +localStorage.getItem("adminId");
+
+    let nothingToShow = ref(false);
+    let loading = ref(false);
+
+    let modalDataId = ref({
+      id: "",
+      change: 0,
+    });
+    let modalDataIdOthers = ref({
+      id: "",
+      change: 0,
+    });
+
+    let allInfo = ref({});
+
+    const toOthersTable = ref({});
+    const toYouTable = ref({});
+    let tableData = ref([]);
+    let toYouTableData = ref([]);
+    toOthersTable.value = {
+      isLoading: true,
+    };
+    toYouTable.value = {
+      isLoading: true,
+    };
+
+    const returned = () => {
+      applicationStatus(store, "RTN").then((statusId) => { 
+        store
+          .dispatch("reviewerRenewal/getRenewalRevoked", statusId)
+          .then((res) => { 
+            allInfo.value = res;
+
+            JSON.parse(JSON.stringify(allInfo.value)).forEach((element) => {
+              if (element.renewalReviewer.reviewerId == adminId) {
+                toYouTableData.value.push({
+                  id: element.id,
+                  ApplicantName:
+                    element.profile.name +
+                    " " +
+                    element.profile.fatherName +
+                    " " +
+                    element.profile.grandFatherName,
+                  ApplicationType: element.applicantType.name,
+                  Date: new Date(element.createdAt)
+                    .toJSON()
+                    .slice(0, 10)
+                    .replace(/-/g, "/"),
+                  data: element,
+                });
+              } else {
+                tableData.value.push({
+                  id: element.id,
+                  ApplicantName:
+                    element.profile.name +
+                    " " +
+                    element.profile.fatherName +
+                    " " +
+                    element.profile.grandFatherName,
+                  ApplicationType: element.applicantType.name,
+                  Date: new Date(element.createdAt)
+                    .toJSON()
+                    .slice(0, 10)
+                    .replace(/-/g, "/"),
+                  data: element,
+                });
+              }
+            });
+
+            toYouTable.value = {
+              columns: [
+                {
+                  label: "ID",
+                  field: "id",
+                  width: "3%",
+                  sortable: true,
+                  isKey: true,
+                },
+                {
+                  label: "Applicant Name",
+                  field: "ApplicantName",
+                  width: "20%",
+                  sortable: true,
+                },
+                {
+                  label: "Applicant Type",
+                  field: "ApplicationType",
+                  width: "15%",
+                  sortable: true,
+                },
+                {
+                  label: "Date",
+                  field: "Date",
+                  width: "15%",
+                  sortable: true,
+                },
+                {
+                  label: "",
+                  field: "quick",
+                  width: "10%",
+                  display: function (row) {
+                    return (
+                      '<button data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="edit-btn bg-primary-700 text-white hover:bg-white hover:text-primary-600 inline-block px-6 py-2.5    font-medium text-xs leading-tight uppercase rounded shadow-md   hover:shadow-lg    transition duration-150 ease-in-out" data-id="' +
+                      row.id +
+                      '" ><i class="fa fa-eye"></i>View/Edit</button>'
+                    );
+                  },
+                },
+              ],
+              rows: JSON.parse(JSON.stringify(toYouTableData.value)),
+              totalRecordCount: toYouTableData.value.length,
+              sortable: {
+                order: "id",
+                sort: "asc",
+              },
+            };
+
+            toOthersTable.value = {
+              columns: [
+                {
+                  label: "ID",
+                  field: "id",
+                  width: "3%",
+                  sortable: true,
+                  isKey: true,
+                },
+                {
+                  label: "Applicant Name",
+                  field: "ApplicantName",
+                  width: "20%",
+                  sortable: true,
+                },
+                {
+                  label: "Applicant Type",
+                  field: "ApplicationType",
+                  width: "15%",
+                  sortable: true,
+                },
+                {
+                  label: "Date",
+                  field: "Date",
+                  width: "15%",
+                  sortable: true,
+                },
+                {
+                  label: "",
+                  field: "quick",
+                  width: "10%",
+                  display: function (row) {
+                    return (
+                      '<button  data-set="' +
+                      row +
+                      '"  data-bs-toggle="modal" data-bs-target="#staticBackdropOthers" class="edit-btn bg-primary-700 text-white hover:bg-white hover:text-primary-600 inline-block px-6 py-2.5    font-medium text-xs leading-tight uppercase rounded shadow-md   hover:shadow-lg    transition duration-150 ease-in-out" data-id="' +
+                      row.id +
+                      '" ><i class="fa fa-eye"></i>View/Edit</button>'
+                    );
+                  },
+                },
+              ],
+              rows: JSON.parse(JSON.stringify(tableData.value)),
+              totalRecordCount: tableData.value.length,
+              sortable: {
+                order: "id",
+                sort: "asc",
+              },
+            };
+          });
+      });
+    };
+
+    const tableLoadingFinish = () => {
+      toOthersTable.value.isLoading = false;
+      toYouTable.value.isLoading = false;
+      let elements = document.getElementsByClassName("edit-btn");
+
+      Array.prototype.forEach.call(elements, function (element) {
+        if (element.classList.contains("edit-btn")) {
+          element.addEventListener("click", rowClicked());
+        }
+      });
+    };
+
+    const tableLoadingFinishOthers = () => {
+      let elements = document.getElementsByClassName("edit-btn-others");
+
+      Array.prototype.forEach.call(elements, function (element) {
+        if (element.classList.contains("edit-btn-others")) {
+          element.addEventListener("click", rowClicked());
+        }
+      });
+      toOthersTable.value.isLoading = false;
+    };
+
+    const rowClicked = (row) => {
+      if (row != undefined) {
+        row = JSON.parse(JSON.stringify(row));
+        modalDataId.value.change++;
+        modalDataId.value.id = row.id ? row.id : "";
+      }
+    };
+
+    const rowClickedOthers = (row) => {
+      if (row != undefined) {
+        row = JSON.parse(JSON.stringify(row));
+        modalDataIdOthers.value.change++;
+        modalDataIdOthers.value.id = row.id ? row.id : "";
+      }
+    };
+
+    onMounted(() => {
+      returned();
+    });
+
+    return {
+      nothingToShow,
+      allInfo,
+      loading,
+      toOthersTable,
+      toYouTable,
+      showModal,
+      tableLoadingFinish,
+      rowClicked,
+      modalDataId,
+      modalDataIdOthers,
+      rowClickedOthers,
+      tableLoadingFinishOthers,
+    };
+  },
+};
+</script>
