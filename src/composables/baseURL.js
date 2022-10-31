9ac2e528d28bdd3792c3f03c39ff89f1e3623c4b
AbelaSW <abela.sol.yo@gmail.com>
2022-10-31 18:59:41 +0300

    UPDATE: BE url

--- src/composables/baseURL.js
+++ src/composables/baseURL.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://hrlicensurebe.dev.k8s.sandboxaddis.com/api";
+export const baseUrl = "https://ihris.moh.gov.et/api";
 export const googleApi = "https://storage.googleapis.com/hris-lisence-dev/";
