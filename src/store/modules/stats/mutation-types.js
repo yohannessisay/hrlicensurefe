60afb89dfec62af51fa2f48d569fc28fa0dbc6c8
Yohannes Sisay <yohannes762@gmail.com>
2023-05-08 11:15:12 +0300

    change admin counter to region based

--- src/store/modules/stats/mutation-types.js
+++
@@ -1,5 +0,0 @@
-export const SET_STATS = "SET_STATS";
-
-export default {
-  SET_STATS
-};
--- src/store/modules/stats/mutations.js
+++
@@ -1,7 +0,0 @@
-import { SET_STATS } from "./mutation-types";
-
-export default {
-  [SET_STATS](state, data) {
-    state.stats = data;
-  },
-};
