fa81be67c2588f2acf79ac00c423bdbd532b3e87
Yohannes Sisay <yohannessisay90@gmail.com>
2023-05-23 17:50:56 +0300

    wingu change base url to wingu backend

--- src/composables/baseURL.js
+++ src/composables/baseURL.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://ihris.moh.gov.et/hrl/api";
+export const baseUrl = "https://wingu.api.hrl.sandboxaddis.com/api";
 export const googleApi = "https://storage.googleapis.com/hris-lisence-dev/";
