e0ae49e20c631845209da72ba878c6d77efbecbc
Yohannes Sisay <yohannessisay90@gmail.com>
2023-08-07 10:45:49 +0300

    base url change

--- src/composables/baseURL.js
+++ src/composables/baseURL.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:5000/api";
+export const baseUrl = "https://hrlicensurebe.dev.k8s.sandboxaddis.com/api";
 export const googleApi = "https://storage.googleapis.com/hris-lisence-dev/";
