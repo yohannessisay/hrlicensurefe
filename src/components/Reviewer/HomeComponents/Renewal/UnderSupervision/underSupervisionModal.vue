64290fe2b0a97d091e66c6781cb9152fbe25cc0e
yohannessisay90@gmail.com <yohannessisay90@gmail.com>
2023-08-28 08:01:46 +0300

    undersupervision letter format change

--- src/components/Reviewer/HomeComponents/Renewal/UnderSupervision/underSupervisionModal.vue
+++ src/components/Reviewer/HomeComponents/Renewal/UnderSupervision/underSupervisionModal.vue
@@ -36,7 +36,7 @@
           text-current
         "
       >
-   <div
+        <div
           class="
             modal-header
             flex flex-shrink-0
@@ -71,7 +71,9 @@
               ease-in-out"
             data-bs-dismiss="modal"
             aria-label="Close"
-          ><i class="fa fa-close fa-2x"></i></button>
+          >
+            <i class="fa fa-close fa-2x"></i>
+          </button>
         </div>
         <div class="vld-parent mt-4">
           <loading
@@ -125,7 +127,7 @@
                                 justify-center
                               "
                             >
-                                 <picture>
+                              <picture>
                                 <source
                                   :srcset="
                                     modalData.profile &&
@@ -427,6 +429,246 @@
       </div>
     </div>
   </div>
+  <div
+    class="
+      modal
+      fade
+      fixed
+      top-0
+      left-0
+      hidden
+      w-full
+      h-full
+      outline-none
+      overflow-x-hidden overflow-y-auto
+    "
+    id="printSupervision"
+    tabindex="-1"
+    aria-labelledby="printSupervisionModalTitle"
+    aria-modal="true"
+    role="dialog"
+  >
+    <div
+      class="
+        modal-dialog modal-dialog-centered
+        modal-lg
+        relative
+        w-auto
+        pointer-events-none
+      "
+    >
+      <div
+        class="
+          modal-content
+          border-none
+          shadow-lg
+          relative
+          flex flex-col
+          w-full
+          pointer-events-auto
+          bg-white bg-clip-padding
+          rounded-md
+          outline-none
+          text-current
+        "
+      >
+        <div
+          class="
+            modal-header
+            flex flex-shrink-0
+            items-center
+            justify-between
+            p-4
+            border-b border-grey-100
+            rounded-t-md
+          "
+        >
+          <h5
+            class="text-xl font-medium leading-normal text-gray-800"
+            id="superviseModalLabel"
+          >
+            Supervision Letter Detail
+          </h5>
+          <button
+            type="button"
+            class="
+              btn-close
+              box-content
+              w-4
+              h-4
+              p-1
+              text-black
+              border-none
+              rounded-none
+              opacity-50
+              focus:shadow-none focus:outline-none focus:opacity-100
+              hover:text-black hover:opacity-75 hover:no-underline
+            "
+            data-bs-dismiss="modal"
+            aria-label="Close"
+            style="min-height: 28px; min-width: 28px"
+          ></button>
+        </div>
+
+        <div class="container shadow-md rounded-lg" contenteditable="true">
+          <div class="px-8 m-4 " id="printSupervisionModal">
+            <div class="grid grid-cols-2 mb-8 mt-48">
+              <div class="flex justify-start" contenteditable="false">
+                <img :src="qrSrc" alt="" style="height: 150px;width: 150px;" />
+              </div>
+            </div>
+
+            <h5 class="mt-4 px-8">ለ________________________</h5>
+            <h5 class="mt-4 px-8">አዲስ አበባ</h5>
+
+            <p class="mt-8 px-8" style="letter-spacing: 2px;">
+              ከዚህ በታች
+              {{
+                modalData &&
+                modalData.data &&
+                modalData.data.profile &&
+                modalData.data.profile.gender == "male"
+                  ? "ስሙ "
+                  : "ስሟ "
+              }}
+              በሰንጠረዥ የተመለከቱት ባለሙያ በቻይና ሀገር ከሚገኘው
+              {{
+                modalData.data && modalData.data.supervisingInstitution
+                  ? modalData.data.supervisingInstitution.name
+                  : ""
+              }}
+              የህክምና
+              {{
+                modalData &&
+                modalData.data &&
+                modalData.data.profile &&
+                modalData.data.profile.gender == "male"
+                  ? "ትምህርቱን ሲከታተል ቆይቶ"
+                  : "ትምህርቷን ስትከታተል ቆይታ"
+              }}
+              ኢንተርንሽፕ ሀገር ውስጥ ለመስራት በቀን17/03/2013ዓ.ም ባቀረቡት ማመልከቻ ጠይቀዋል፡፡ በዚህም
+              መሠረት ጉዳያቸው በ __/__/__ ዓ.ም ቀርቦ ከታየ ከተገመገመ በኃላ To Work Rotating
+              Medical Intern for One Years በሚል ፀድቆቸው ተመዝገበዋል፡፡ ስለዚህም ልምምዱን ከ
+              18/3/2013ዓ.ም ጀምሮ ለአንድ አመት እንዲከታተሉ የተመደቡ መሆኑን እየገለጽን ሆስፒታሉ በአንድ አመት
+              መጨረሻ አስፈላጊውን ሁሉ በመፈጸም
+              {{
+                modalData &&
+                modalData.data &&
+                modalData.data.profile &&
+                modalData.data.profile.gender == "male"
+                  ? "የባለሙያዉን "
+                  : "የባለሙያዋን"
+              }}
+              ውጤቱን እንድታሳውቁን እንጠይቃለን፡፡
+            </p>
+
+            <div class="grid justify-items-center mt-8">
+              <table class=" border text-center text-sm font-light ">
+                <thead class="border-b font-medium bg-primary-200 ">
+                  <tr>
+                    <th scope="col" class="border-r px-6 py-4 ">
+                      #ተ.ቁ
+                    </th>
+                    <th
+                      scope="col"
+                      class="border-r px-6 py-4 dark:border-neutral-500"
+                    >
+                      የስም ዝርዝር
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="border-b dark:border-neutral-500">
+                    <td
+                      class="whitespace-nowrap border-r px-6 py-4 font-medium dark:border-neutral-500"
+                    >
+                      1
+                    </td>
+                    <td
+                      contenteditable="false"
+                      class="whitespace-nowrap border-r px-6 py-4"
+                    >
+                      <span class="text-yellow-300">
+                        {{
+                          modalData && modalData.data && modalData.data.profile
+                            ? modalData.data.profile.name +
+                              " " +
+                              modalData.data.profile.fatherName +
+                              " " +
+                              modalData.data.profile.grandFatherName
+                            : []
+                        }}
+                      </span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <h5 class="mt-8 mb-48 text-right">//ከሰላምታ ጋር//</h5>
+          </div>
+        </div>
+
+        <div
+          class="
+            modal-footer
+            flex flex-shrink-0 flex-wrap
+            items-center
+            justify-end
+            p-1
+            border-t border-grey-100
+            rounded-b-md
+          "
+        >
+          <button
+            type="button"
+            class="
+              inline-block
+              px-6
+              py-2.5
+              bg-primary-700
+              text-white
+              font-medium
+              text-xs
+              leading-tight
+              uppercase
+              rounded
+              shadow-lg
+              hover:bg-white hover:text-primary-600
+              transition
+              duration-150
+              ease-in-out
+            "
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button
+            class="
+              inline-block
+              px-6
+              py-2.5
+              bg-primary-700
+              text-white
+              font-medium
+              text-xs
+              leading-tight
+              uppercase
+              rounded
+              shadow-lg
+              hover:bg-white hover:text-primary-600
+              transition
+              duration-150
+              ease-in-out
+            "
+            @click="print()"
+          >
+            Print
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import { useStore } from "vuex";
@@ -435,7 +677,8 @@ import moment from "moment";
 import Loading from "vue3-loading-overlay";
 import "vue3-loading-overlay/dist/vue3-loading-overlay.css";
 import { googleApi } from "@/composables/baseURL";
-
+import html2pdf from "html2pdf.js";
+import STATIC_CERTIFICATE_URL from "../../../../../sharedComponents/constants/message.js";
 export default {
   props: ["modalDataId"],
   components: {
@@ -446,7 +689,7 @@ export default {
   },
   setup(props) {
     const store = useStore();
-
+    let qrSrc = ref("");
     let show = ref(true);
     let adminId = +localStorage.getItem("adminId");
 
@@ -463,7 +706,67 @@ export default {
     };
     const modalData = ref({});
     let result = {};
+    const print = async () => {
+      var data = document.getElementById("printSupervisionModal");
+      const staticUrl = STATIC_CERTIFICATE_URL;
+      const userId = modalData.value.data.profile.id;
+      const applicationId = modalData.value.data.id;
+      const applicationType = "Renewal";
 
+      const qrParam = { url: null };
+
+      qrParam.url =
+        staticUrl + "/" + applicationType + "/" + userId + "/" + applicationId;
+      store
+        .dispatch("reviewer/getQrCode", qrParam)
+        .then((res) => {
+          qrSrc.value = res.data.data;
+        })
+        .finally(async () => {
+          var element = data;
+          var opt = {
+            margin: 1,
+            filename: "myfile.pdf",
+            image: { type: "jpeg", quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { orientation: "p", unit: "mm", format: "a4" },
+          };
+
+          html2pdf()
+            .set(opt)
+            .from(element)
+            .save(
+              modalData.value.data &&
+                modalData.value.data.profile &&
+                modalData.value.data.profile.name
+                ? modalData.value.data.profile.name +
+                    " " +
+                    new Date().toISOString().slice(0, 10)
+                : ""
+            )
+            .then(() => {
+              document.getElementById("applicantCopyHeader").style.display =
+                "none";
+              document.getElementById("applicantCopyFooter").style.display =
+                "none";
+              html2pdf()
+                .set(opt)
+                .from(document.getElementById("printSupervisionModal"))
+                .save(
+                  modalData.value.data &&
+                    modalData.value.data.profile &&
+                    modalData.value.data.profile.name
+                    ? modalData.value.data.profile.name +
+                        " " +
+                        new Date().toISOString().slice(0, 10)
+                    : ""
+                );
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
     const check = () => {
       store
         .dispatch("reviewer/getNewLicenseApplication", props.modalDataId.id)
@@ -499,7 +802,7 @@ export default {
             modalData.value.email = result.applicant.emailAddress
               ? result.applicant.emailAddress
               : "-----";
-     
+
             modalData.value.profile = result.profile;
             modalData.value.professionalTypes = result.licenseProfessions;
             modalData.value.certifiedDate = result.certifiedDate;
@@ -522,6 +825,7 @@ export default {
       adminId,
       reviewerAdminId,
       showModal,
+      print,
       check,
       isLoading,
       onCancel,
