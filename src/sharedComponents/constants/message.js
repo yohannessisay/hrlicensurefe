23cb6eb2e7cc7388b3a1dae499a20304754cf3ce
yohannessisay90@gmail.com <yohannessisay90@gmail.com>
2023-10-02 16:39:14 +0300

    change url's to sandbox

--- src/sharedComponents/constants/message.js
+++ src/sharedComponents/constants/message.js
@@ -1,3 +1,2 @@
-const STATIC_CERTIFICATE_URL = "https://hrl.moh.gov.et/scanned-certified-user";
-
+const STATIC_CERTIFICATE_URL = "https://hrlicensurefe.dev.k8s.sandboxaddis.com/scanned-certified-user";
 export default STATIC_CERTIFICATE_URL;
